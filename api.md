0e42d79a6746d377f025df9e2f3d155702546ec3
Matheus Henrique Da Silva <matheushenriquedasilvaa.2021@gmail.com>
2022-03-08 18:30:41 -0300

    api com express

---
+++ api.md
@@ -0,0 +1,8 @@
+# Ativiade prática✨
+
+__Criar uma endpoint para users💁‍♂️:__
+
+* Listar usuários (GET)
+* Criar usuário (POST)
+* Modificar usuário (PUT)
+* Remover usuário (DELETE)
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const userRoute = require("./routes/userRoute");
+
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+userRoute(app);
 
 app.get("/", (req, res) => {
   res.send("Olá mundo pelo Express!");
